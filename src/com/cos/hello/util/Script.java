02a26714efa0f966bc99dee5e508d2aeaa38d452
oshLunch <tmdgns5236@naver.com>
2020-12-24 17:06:46 +0900

    한글깨짐 수정

--- src/com/cos/hello/util/Script.java
+++ src/com/cos/hello/util/Script.java
@@ -5,18 +5,19 @@ import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
 
-public class Script {
-
+public class Script {	
 	public static void back(HttpServletResponse resp, String msg) throws IOException {
+		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = resp.getWriter();
 		out.print("<script>");
-		out.print("alert('\"+msg+\"');");	//경고창
+		out.print("alert('"+msg+"');");	//경고창
 		out.print("history.back();");		//뒤로가기
 		out.print("</script>");
 		out.flush();
 	}
 	
 	public static void href(HttpServletResponse resp, String url, String msg) throws IOException {
+		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = resp.getWriter();
 		out.print("<script>");
 		out.print("alert('"+msg+"');");	//경고창
